2fdd42cea118c819fdafacefd3fda64bc4193a33
Kel <kel@simplykel.ru>
2023-04-20 10:58:24 +0300

    Update files & directory

---
+++ src/main/java/org/opinionloop/motd/Config.java
@@ -0,0 +1,83 @@
+package org.opinionloop.motd;
+
+import lombok.Getter;
+import net.minecraft.SharedConstants;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Config {
+
+    // Чтобы можно было менять в 1 месте и не вспоминать где нужно изменить.
+    private final int defaultLineCount = 58;
+    private final Path configFile = Main.getServer().getRunDirectory().toPath().resolve("config/smotd.json");
+    @Getter
+    private String line1 = "A Minecraft Server";
+    @Getter
+    private String line2 = "Minecraft " + SharedConstants.getGameVersion().getName();
+    @Getter
+    private String day = "Day";
+    @Getter
+    private String night = "Night";
+    @Getter
+    private String morning = "Morning";
+    @Getter
+    private String evening = "Evening";
+    @Getter
+    private int lineCount = defaultLineCount;
+    @Getter
+    private Boolean useRandomLine2 = false;
+    @Getter
+    private JSONArray randomLine2 = new JSONArray().put("Hello, world!").put("Hi!");
+
+    public Config() {
+        if (!configFile.toFile().exists()) {
+            save();
+            Main.getLog().info(Main.prefix + "Please reload server for load my config");
+            Main.getServer().exit();
+        } else {
+            try {
+                JSONObject json = new JSONObject(Files.readString(configFile, StandardCharsets.UTF_8));
+                for (String key : json.keySet()) {
+                    switch (key) {
+                        case "line1" -> line1 = json.getString(key);
+                        case "line2" -> line2 = json.getString(key);
+                        case "day" -> day = json.getString(key);
+                        case "night" -> night = json.getString(key);
+                        case "morning" -> morning = json.getString(key);
+                        case "evening" -> evening = json.getString(key);
+                        case "countLine" -> lineCount = json.getInt(key);
+                        case "useRandomLine2" -> useRandomLine2 = json.getBoolean(key);
+                        case "randomLine2" -> randomLine2 = json.getJSONArray(key);
+                        default -> {
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void save() {
+        JSONObject json = new JSONObject();
+        json.put("line1", "A Minecraft Server")
+                .put("line2", "Minecraft " + SharedConstants.getGameVersion().getName())
+                .put("day", "Day")
+                .put("night", "Night")
+                .put("morning", "Morning")
+                .put("evening", "Evening")
+                .put("countLine", defaultLineCount)
+                .put("useRandomLine2", false)
+                .put("randomLine2", new JSONArray().put("Hello, world!").put("Hi!"));
+        try {
+            Files.createDirectories(configFile.getParent());
+            Files.writeString(configFile, json.toString(), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/opinionloop/motd/Main.java
@@ -0,0 +1,60 @@
+package org.opinionloop.motd;
+
+import lombok.Getter;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main implements ModInitializer {
+    public static final String MOD_ID = "smotd";
+    public static final String prefix = "[" + MOD_ID + "] ";
+    private static Config config;
+    @Getter
+    private static final Logger log = LogManager.getLogger(MOD_ID);
+    @Getter
+    private static MinecraftServer server;
+
+    @Override
+    public void onInitialize() {
+        ServerLifecycleEvents.SERVER_STARTED.register(this::start);
+    }
+
+    private void start(MinecraftServer minecraftServer) {
+        log.info(prefix + "sMOTD started");
+        server = minecraftServer;
+        config = new Config();
+        setMetaData();
+    }
+
+    public static void setMetaData() {
+        // Инициализация описания
+        String descreption = Utils.fixFormatCodes(config.getLine1());
+
+        // Подсчет времени. Если мир - null, ставит -1
+        ServerWorld world = server.getWorld(World.OVERWORLD);
+        long dayTime = world != null ? world.getLunarTime() % 24000L : -1;
+
+        String time = dayTime < 0 ? "" : dayTime < 6000 ? config.getMorning() : dayTime < 12000 ?
+                config.getDay() : dayTime < 16500 ? config.getEvening() : config.getNight();
+
+        // Добавление типа времени, если время > 0
+        if (time.length() != 0) {
+            int countEnable = config.getLineCount() - Utils.clearFormatCodes(config.getLine1()).length() -
+                    Utils.clearFormatCodes(time).length();
+            descreption = countEnable <= 0 ? Utils.fixFormatCodes(config.getLine1()) :
+                    Utils.fixFormatCodes(config.getLine1() + " ".repeat(countEnable) + time);
+        }
+
+        // В случае, если НЕ рандомная строка на 2 линии, происходит добавление 2 строки. Иначе: выбор рандомной строки.
+        double random = Math.floor(Math.random() * config.getRandomLine2().length());
+        descreption += !config.getUseRandomLine2() ? "\n" + Utils.fixFormatCodes(config.getLine2()) :
+                config.getRandomLine2().length() == 0 ? "\n" + Utils.fixFormatCodes(config.getLine2()) :
+                        "\n" + Utils.fixFormatCodes(config.getRandomLine2().getString((int) random));
+
+        server.setMotd(descreption);
+    }
+}
